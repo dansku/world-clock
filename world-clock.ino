1494f124c08f76a9d138ff4c8d42e0b90fa67536
Daniel Andrade <daniel@danielandrade.net>
2015-03-18 23:07:51 +1100

    Add RTC Hardware code.

--- world-clock.ino
+++ world-clock.ino
@@ -1,12 +1,14 @@
 /*
 
- Dani's New Awesome Clock
- 
- */
+	Dani's New Awesome Clock
+	Code rewrite
 
-// LCD Config
-#include "U8glib.h"
-U8GLIB_ST7920_128X64_1X u8g(6,5,4,7);
+*/
+
+//RTC Library
+#include <Wire.h>		// Wire Library
+#include "ds3231.h"		// RTC Library
+struct ts t;
 
 // DHT22
 #include "DHT.h"
@@ -14,78 +16,99 @@ U8GLIB_ST7920_128X64_1X u8g(6,5,4,7);
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
+// LCD Config
+#include "U8glib.h"
+U8GLIB_ST7920_128X64_1X u8g(6,5,4,7);
 
+uint8_t secset = 0; 	//Index for second RTC setting
+uint8_t minset = 1; 	//Index for minute RTC setting
+uint8_t hourset = 2; 	//Index for hour RTC setting
+uint8_t wdayset = 3; 	//Index for weekday RTC setting
+uint8_t mdayset = 4; 	//Index for date RTC setting
+uint8_t monset = 5; 	//Index for month RTC setting
+uint8_t yearset = 6;	 //Index for year RTC setting
 
-// Start Time
-int second = 51, minute = 59, hour = 13;
-
-char BRhours[3];
 
 void setup(){
-
-  // Start Serial
-  Serial.begin(9600);
-  Serial.println("Starting...");
-
-  //DHT Start
+    Serial.begin(9600);				// Start serial
+    Wire.begin();					// Start wire library
+    DS3231_init(DS3231_INTCN);		// Start RTC library
+    
+      //DHT Start
   dht.begin();
-
+  
+    //If you want to set the time on the RTC librat, uncomment here:
+//	t.mday = 18;
+//	t.mon = 3;
+//	t.hour = 23;
+//	t.min = 3; 
+//	t.year = 2015;
+//	set_rtc_field(t, hourset);
+//	set_rtc_field(t, minset);
+//	set_rtc_field(t, yearset);
+//        set_rtc_field(t, mdayset);
+//        set_rtc_field(t, monset);
 }
 
 void loop(){
-  //Let' draw
-  u8g.firstPage();  
-  do { draw(); } 
-  while( u8g.nextPage() );
-  delay(50);
+  
+
+
+	//Let' draw
+	u8g.firstPage();  
+	do { draw(); } 
+	while( u8g.nextPage() );
+	delay(100);
 }
 
-// Char for regular clock
-char hr[2],  mins[2], secs[2];
 
-// Char for Other Countries
-char BRh[2], USh[2], FRh[2], FIh[2];
+void draw(void){
 
-void draw(void) {
-  
-int BRdiff = 14;
-int USdiff = 16;
-int FRdiff = 10;
-int FIdiff = 9;
+	/* Here the magic happens */
 
-int hourBR = 0;
-int hourUS = 0;
-int hourFI = 0;
-int hourFR = 0;
+	DS3231_get(&t); // grab the time from the RTC
+	int second	= t.sec;
+	int minute	= t.min;
+	int hour	= t.hour;
+	int day 	= t.mday;
+	int month	= t.mon;
+	int year	= t.year;
 
-  // Silly code to fake hour ---- 
-  static unsigned long lastTick = 0; 
-  if (millis() - lastTick >= 1000) {
-    lastTick = millis();
-    second++;
-  }
+	/* Country fuse difference */
 
-  if (second >= 60) { minute++;  second = 0; } // reset seconds to zero 
-  if (minute >=60) { hour++; minute = 0; }     // reset minutes to zero
-  if (hour >=24) { hour=0; minute = 0; }       // reset minutes to zero
-  
-    // Time Melbourne
-  int y = 4; // Move
-  char hours[3] = {(hour / 10) + 48, (hour % 10) + 48, 0};
-  char minutes[3] = {(minute / 10) + 48, (minute % 10) + 48, 0};
-  char seconds[3] = {(second / 10) + 48, (second % 10) + 48, 0};
-  
-  u8g.setFont(u8g_font_freedoomr10r);
-  u8g.drawStr( 5+y, 15, hours);
-  u8g.drawStr( 18+y, 15,":");
-  u8g.drawStr( 24+y, 15, minutes);
-  u8g.drawStr( 37+y, 15,":");
-  u8g.drawStr( 43+y, 15, seconds);
+	int BRdiff = 14;
+	int USdiff = 15;
+	int FRdiff = 10;
+	int FIdiff = 9;
 
-  // Date
-  u8g.drawStr( 7, 30,"22/02/15"); 
-  
-  // Here the magic happens
+	int hourBR;
+	int hourUS;
+	int hourFI;
+	int hourFR;
+
+	// Time Melbourne
+	int y = 4; // Move
+	char hours[3] 	= {(hour / 10) + 48, (hour % 10) + 48, 0};
+	char minutes[3] = {(minute / 10) + 48, (minute % 10) + 48, 0};
+	char seconds[3] = {(second / 10) + 48, (second % 10) + 48, 0};
+	char years[3] = {((year-2000) / 10) + 48, ((year-2000) % 10) + 48, 0};
+	char months[3]	= {(month / 10) + 48, (month % 10) + 48, 0};
+	char days[3]	= {(day / 10) + 48, (day % 10) + 48, 0};
+
+	u8g.setFont(u8g_font_freedoomr10r);
+	u8g.drawStr( 5+y,  15, hours);
+	u8g.drawStr( 18+y, 15,":");
+	u8g.drawStr( 24+y, 15, minutes);
+	u8g.drawStr( 37+y, 15,":");
+	u8g.drawStr( 43+y, 15, seconds);
+
+	// Date Melbourne
+	u8g.drawStr( 7,  30, days);
+	u8g.drawStr( 22, 30, "/");
+	u8g.drawStr( 31, 30, months);
+	u8g.drawStr( 46, 30, "/");
+	u8g.drawStr( 50, 30, years );
+ // Here the magic happens
   u8g.setFont(u8g_font_6x12);
 
   // Temperature
@@ -143,9 +166,40 @@ int hourFR = 0;
   u8g.drawStr( 25, 60, hoursFI);
   u8g.drawStr( 37, 60,":");
   u8g.drawStr( 42, 60, minutes);
-
+ 
 }
 
 
 
+//Subroutine to adjust a single date/time field in the RTC
+void set_rtc_field(struct ts t,  uint8_t index)
+{
+    uint8_t century;
 
+    if (t.year > 2000) {
+        century = 0x80;
+        t.year_s = t.year - 2000;
+    } else {
+        century = 0;
+        t.year_s = t.year - 1900;
+    }
+
+    uint8_t TimeDate[7] = { t.sec, t.min, t.hour, t.wday, t.mday, t.mon, t.year_s };
+
+    Wire.beginTransmission(DS3231_I2C_ADDR);
+    Wire.write(index);
+        TimeDate[index] = dectobcd(TimeDate[index]);
+        if (index == 5){TimeDate[5] += century;}
+        Wire.write(TimeDate[index]);
+    Wire.endTransmission();
+    
+    //Adjust the month setting, per data sheet, if the year is changed
+    if (index == 6){
+      Wire.beginTransmission(DS3231_I2C_ADDR);
+      Wire.write(5);
+      TimeDate[5] = dectobcd(TimeDate[5]);
+      TimeDate[5] += century;
+      Wire.write(TimeDate[5]);
+      Wire.endTransmission();
+    } 
+}
